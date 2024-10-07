c8452d869341cd256feba3bfbb56dae3c0357f2b
Amr Khaled <163432220+AMR-bsheer@users.noreply.github.com>
2024-10-07 13:42:19 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function login() {
+    alert('تم النقر على زر تسجيل الدخول.');
+}
+
+function signup() {
+    alert('تم النقر على زر إنشاء حساب.');
+}
+
+function exploreCourses() {
+    alert('تم النقر على زر استكشاف الدورات.');
+}
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('تم إرسال النموذج بنجاح!');
+});
+
+function login() {
+    alert("Login functionality to be implemented.");
+}
+
+function signup() {
+    alert("Signup functionality to be implemented.");
+}
+
